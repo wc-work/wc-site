159ec265f24ed22052455fdac4a1109dfb85c96f
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-01-22 13:28:03 -0800

    inital commit

--- wp-cli/browse-command.php
+++ wp-cli/browse-command.php
@@ -23,9 +23,9 @@ $browse_command = function( $_, $assoc_args ) {
 	$wp_admin = \WP_CLI\Utils\get_flag_value( $assoc_args, 'wp-admin' );
 
 	if ( true === $wp_admin ) {
-		$url = 'http://webcrumbs.site/wp-admin/';
+		$url = 'http://vccw.test/wp-admin/';
 	} else {
-		$url = 'http://webcrumbs.site/';
+		$url = 'http://vccw.test/';
 	}
 
 	passthru( $exec . ' ' . escapeshellarg( $url ) );
