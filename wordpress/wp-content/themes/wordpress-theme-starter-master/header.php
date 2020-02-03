c004e48e398d70eadef14854ee26af3ab30754c1
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-02-03 15:45:08 -0800

    added placer image for mobile. will need to determine media-query

--- wordpress/wp-content/themes/wordpress-theme-starter-master/header.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/header.php
@@ -30,15 +30,5 @@
 
 			<!-- header -->
 			<header class="header clear" role="banner">
-			
-				<!-- logo -->
-				<div class="logo">
-					<a href="<?php echo home_url(); ?>">
-						<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-						<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-					</a>
-				</div>
-				<!-- /logo -->
-
 			</header>
 			<!-- /header -->
---
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-about-us.php
@@ -0,0 +1,20 @@
+<?php 
+/* 
+ * Template Name: About Us Page
+ * Template Post Type: page
+ */
+get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+            <div class="banner container-fluid">
+                <img src="http://webcrumbs.site/wp-content/uploads/2020/02/WebCrumbs-Group-Photo-JPEG-scaled.jpg">
+            </div>
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
