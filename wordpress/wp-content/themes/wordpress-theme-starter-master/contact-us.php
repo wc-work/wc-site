1c732c683d75eb979a7dbf351957399caf0e5341
SolNat99 <natalieas01@gmail.com>
2020-02-03 10:15:47 -0800

    Inital commit

---
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
@@ -0,0 +1,31 @@
+<?php 
+/*
+ * Template Name: ContactPage
+ * Template Post Type: page
+ */
+ get_header(); ?>
+
+
+<!--Contact Info Row-->
+<div class="container">
+
+<div class="row">
+    <div class="col-sm">
+       <h2> Location </h2>
+    </div>
+
+    <div class="col-sm">
+        <h2> Email </h2>
+    </div>
+
+    <div class="col-sm">
+        <h2> </h2>
+    </div>
+
+</div><!--end of row -->
+
+</div><!--end container -->
+
+
+
+<?php get_footer(); ?>
