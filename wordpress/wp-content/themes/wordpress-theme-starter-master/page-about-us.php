61a6067a8b5dc788e60b3e78251d0d6d1b9abaf6
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-02-04 11:22:51 -0800

    adjust css grid for quotations

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-about-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-about-us.php
@@ -12,23 +12,25 @@ get_header(); ?>
             </div>
         </section>
         <section>
-            <div class="box grid">
-                <div class="grid-temp">
-                    <div class="item1">
-                        <picture class="picback">
-                            <img src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-left-quotation.png">
-                        </picture>
-                    </div>
-                    <div class="quotation item2">
-                        <p>At WebCrumbs, we always make sure to have our HAT on...</p>
-                    </div>
-                    <div class="item3">
-                        <picture class="picback">
-                            <img src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-right-quotation.png">
-                        </picture>
-                    </div>
-                </div>          
-            </div>
+            <div class="box">
+                <div class="grid">
+                    <div class="grid-temp">
+                        <div class="item1">
+                            <picture class="picback">
+                                <img src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-left-quotation.png">
+                            </picture>
+                        </div>
+                        <div class="quotation item2">
+                            <p>At WebCrumbs, we always make sure to have our HAT on...</p>
+                        </div>
+                        <div class="item3">
+                            <picture class="picback">
+                                <img src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-right-quotation.png">
+                            </picture>
+                        </div>
+                    </div>          
+                </div>
+            </div>      
         </section>
 
 	</main>
