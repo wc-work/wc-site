6c8f5e9c528c74914f8883a5e713648f14eec44d
SolNat99 <natalieas01@gmail.com>
2020-02-03 16:52:10 -0800

    Started using grid

--- wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
@@ -11,15 +11,20 @@
 
 <div class="row">
     <div class="col-sm">
-       <h2> Location </h2>
+    <?php echo get_theme_file_uri('/images/bread.jpg'); ?>
+       <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
     </div>
 
     <div class="col-sm">
-        <h2> Email </h2>
+        <h2> info.gkw.wp@gmail.com </h2>
     </div>
 
     <div class="col-sm">
-        <h2> </h2>
+        <h2> ###-###-####</h2>
+    </div>
+
+    <div class="col-sm">
+        <h2>Have ideas? Send them to Us!</h2>
     </div>
 
 </div><!--end of row -->
