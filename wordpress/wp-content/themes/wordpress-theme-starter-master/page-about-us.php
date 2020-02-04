4c1662a0d876dbaed0d6dbf68a0be79f5a13b443
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-02-04 10:22:22 -0800

    added css grid to quotation box. still working on functionality

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-about-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-about-us.php
@@ -12,10 +12,22 @@ get_header(); ?>
             </div>
         </section>
         <section>
-            <div class="box">
-                <div class="quotation">
-                    <p>At WebCrumbs, we always make sure to have our HAT on...</p>
-                </div>
+            <div class="box grid">
+                <div class="grid-temp">
+                    <div class="item1">
+                        <picture class="picback">
+                            <img src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-left-quotation.png">
+                        </picture>
+                    </div>
+                    <div class="quotation item2">
+                        <p>At WebCrumbs, we always make sure to have our HAT on...</p>
+                    </div>
+                    <div class="item3">
+                        <picture class="picback">
+                            <img src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-right-quotation.png">
+                        </picture>
+                    </div>
+                </div>          
             </div>
         </section>
 
