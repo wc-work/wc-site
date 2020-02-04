a2c7e750fc5212b5ecd51b97a707a54c223e91eb
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-02-03 16:37:38 -0800

    added white box, need to adjust text height and alignment

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-about-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-about-us.php
@@ -6,13 +6,19 @@
 get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
 		<section>
             <div class="banner container-fluid">
                 <img src="http://webcrumbs.site/wp-content/uploads/2020/02/WebCrumbs-Group-Photo-JPEG-scaled.jpg">
             </div>
-		</section>
-		<!-- /section -->
+        </section>
+        <section>
+            <div class="box">
+                <div class="quotation">
+                    <p>At WebCrumbs, we always make sure to have our HAT on...</p>
+                </div>
+            </div>
+        </section>
+
 	</main>
 
 <?php get_sidebar(); ?>
