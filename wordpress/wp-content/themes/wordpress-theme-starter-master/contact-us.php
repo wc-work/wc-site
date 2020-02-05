b1984f41a749fe109259462aea9dfc1d6a28d122
SolNat99 <natalieas01@gmail.com>
2020-02-04 16:55:33 -0800

    Trying to set up css grid

--- wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
@@ -7,31 +7,25 @@
 
 
 <!--Contact Info Row-->
-<div class="container">
+<div class="box">
+    <div class="grid">
+        <div class="grid-temp">
+            
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
+            <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
+    
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
+            <h2> info.gkw.wp@gmail.com </h2>
 
-<div class="row">
-    <div class="col-sm">
-    <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
-       <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
-    </div>
+            <img class="icons" src="<?php bloginfo('template_url');?>/img/icons/phone.png">
+            <h2> ###-###-####</h2>
 
-    <div class="col-sm">
-    <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
-        <h2> info.gkw.wp@gmail.com </h2>
-    </div>
+            <h2>Have ideas? Send them to Us!</h2>
+    
 
-    <div class="col-sm">
-    <img class="icons" src="<?php bloginfo('template_url');?>/img/icons/phone.png">
-        <h2> ###-###-####</h2>
-    </div>
-
-    <div class="col-sm">
-        <h2>Have ideas? Send them to Us!</h2>
-    </div>
-
-</div><!--end of row -->
-
-</div><!--end container -->
+        </div>
+    </div><!--end grid -->
+</div><!--end box -->
 
 
 
