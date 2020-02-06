c6dc9220efa0f2aa504db9ffa655850c88b870f5
SolNat99 <natalieas01@gmail.com>
2020-02-05 16:50:53 -0800

    Gonna switch to working with bootstrap

--- wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
@@ -7,26 +7,31 @@
 
 
 <!--Contact Info Row-->
-<div class="box">
-    <div class="grid">
-        <div class="grid-temp">
-            
-            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
-            <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
-    
-            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
-            <h2> info.gkw.wp@gmail.com </h2>
+<div class="container">
+
+    <div class="row">
+                <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
+
+                <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
+        
+
+                <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
+     
+
+          
+                <h2> info.gkw.wp@gmail.com </h2>
+         
+
+          
 
             <img class="icons" src="<?php bloginfo('template_url');?>/img/icons/phone.png">
+           
             <h2> ###-###-####</h2>
+         
 
             <h2>Have ideas? Send them to Us!</h2>
-    
+    </div><!-- end row -->
 
-        </div>
-    </div><!--end grid -->
-</div><!--end box -->
-
-
+</div><!--end container-->
 
 <?php get_footer(); ?>
