0d663c55977558f8e9adff057830e62fa2d353cf
SolNat99 <natalieas01@gmail.com>
2020-02-06 16:53:50 -0800

    Got Icons in right spot, now working on button

--- wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
@@ -10,27 +10,47 @@
 <div class="container">
 
     <div class="row">
-                <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
 
-                <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
-        
+        <div class="col-sm-8 d-flex justify-content-center"> 
+             <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
+        </div>  
 
-                <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
-     
-
-          
-                <h2> info.gkw.wp@gmail.com </h2>
-         
-
-          
-
-            <img class="icons" src="<?php bloginfo('template_url');?>/img/icons/phone.png">
-           
-            <h2> ###-###-####</h2>
-         
-
-            <h2>Have ideas? Send them to Us!</h2>
+        <div class="col-sm-4 d-flex justify-content-center">  
+            <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
+        </div>       
     </div><!-- end row -->
+
+
+    <div class="row">
+
+        <div class="col-sm-8 d-flex justify-content-center">
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
+        </div>
+        
+        <div class="col-sm-4 d-flex justify-content-center">
+                <h2> info.gkw.wp@gmail.com </h2>
+        </div>
+    </div><!-- end row -->     
+
+          
+    <div class="row">
+        <div class="col-sm-8 d-flex justify-content-center">
+            <img class="icons" src="<?php bloginfo('template_url');?>/img/icons/phone.png">
+        </div>   
+
+        <div class="col-sm-4 d-flex justify-content-center">
+            <h2> ###-###-####</h2>
+        </div>    
+            
+    </div><!-- end row -->   
+
+    <div class="row">
+        <div class="col-sm-8 d-flex justify-content-center">
+            <h2>Have ideas? Send them to Us!</h2>
+        </div>
+    </div><!-- end row -->  
+
+    <button type="button" class="btn btn-primary btn-lg" type="submit" value="Submit" >Submit </button>
 
 </div><!--end container-->
 
