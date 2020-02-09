fc8e016c1aad4baf012bf3a5659d67be795b9b17
faith orozco <faith_orozco@yahoo.com>
2020-02-08 21:20:34 -0800

    Placed video on homepage

--- wordpress/wp-content/themes/wordpress-theme-starter-master/index.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/index.php
@@ -2,7 +2,11 @@
 
 	<main role="main">
 		<!-- section -->
-		
+		<div class="mov" data-src="<?php echo get_template_directory_uri(); ?>/img/webcrumbs-video.mp4" data-video="<?php echo get_template_directory_uri(); ?>/img/webcrumbs-video.mp4" data-placeholder="<?php echo get_template_directory_uri(); ?>/img/webcrumbs-video.mp4">
+			<video autoplay="" muted="" loop="" poster="<?php echo get_template_directory_uri(); ?>/img/webcrumbs-video.mp4">
+			<source src="<?php echo get_template_directory_uri(); ?>/img/webcrumbs-video.mp4" type="video/mp4">
+			</video>
+      </div>
 		<!-- /section -->
 	</main>
 
