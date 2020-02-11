b5d2f0e426f109747b64f545ca553c0eecd62762
SolNat99 <natalieas01@gmail.com>
2020-02-10 16:50:39 -0800

    Page looks good in all views

--- wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
@@ -7,32 +7,36 @@ get_header(); ?>
 
 
 <!--Contact Info Rows-->
+<div class="container-fluid">
+    <div class="row row1">
 
-<!-- icon-text-container is to help align the icons and text together -->
-<div class="icon-text-container"> 
-    <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
-    <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
-</div>
-
-
-<div class="icon-text-container">
-    <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
-    <h2> info.gkw.wp@gmail.com </h2>
-</div>
+        <!-- icon-text-container is to help align the icons and text together -->
+        <div class="icon-text-container col-xs col-lg-4">
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
+            <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
+        </div>
 
 
-
-<div class="icon-text-container">
-    <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/phone.png">
-    <h2> ###-###-####</h2>
-</div>
+        <div class="icon-text-container col-xs col-lg-4">
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
+            <h2> info.gkw.wp@gmail.com </h2>
+        </div>
 
 
 
-<div class="icon-text-container">
-    <h2>Have ideas? Send them to Us!</h2>
-    <button type="button" class="btn btn-primary btn-lg">Send Ideas</button>
-</div>
+        <div class="icon-text-container col-xs col-lg-4">
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/phone.png">
+            <h2> ###-###-####</h2>
+        </div>
 
+
+
+        <div class="icon-text-container col-xs col-12 text-center">
+            <h2>Have ideas? Send them to Us!</h2>
+            <button type="button" class="btn btn-primary btn-lg">Send Ideas</button>
+        </div>
+
+    </div> <!-- end row -->
+</div> <!-- end container -->
 
 <?php get_footer(); ?>
