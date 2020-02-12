e8da9edaf612c7c1d2e574887507c8f6ffafec2d
faith orozco <faith_orozco@yahoo.com>
2020-02-12 15:23:23 -0800

    implemented navwalker. need to add crumbs and change hamburger

--- wordpress/wp-content/themes/wordpress-theme-starter-master/header.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/header.php
@@ -26,59 +26,41 @@
 	</head>
 	<body <?php body_class(); ?>>
 	
-		<!-- wrapper -->
-		<div class="wrapper">
-
-			<!-- header -->
-			<header class="header clear" role="banner">
-			<nav class="navbar navbar-expand-lg">
-        		<a class="navbar-brand" href="#">
-					<img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/wc-site-logo.png"  alt="WebCrumbs logo written with digital crumbs falling under letters">
-				</a>
-        		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		  <!-- <span class="navbar-toggler-icon"></span> -->
-		  <span class="navbar-toggler-icon">   
-    <i class="fa fa-navicon" style="color:#fff; font-size:28px;"></i>
-</span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav pull-right ml-auto mr-5 mt-2">
-            <li class="nav-item active">
-				<div class="d-flex">
-					<img class="navcrumb" src="<?php echo get_template_directory_uri(); ?>/img/crumb1.png" alt="digital bread crumb">
-				
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-			  	</div>
-            </li>
-            <li class="nav-item">
-				<div class="d-flex">
-					<img class="navcrumb" src="<?php echo get_template_directory_uri(); ?>/img/crumb2.png" alt="digital bread crumb">
-					
-              <a class="nav-link" href="#">About Us</a>
-			  	</div>
-            </li>
-            <li class="nav-item">
-			<div class="d-flex">
-					<img class="navcrumb" src="<?php echo get_template_directory_uri(); ?>/img/crumb3.png" alt="digital bread crumb">
-              <a class="nav-link" href="#">Process</a>
-			  </div>
-            </li>
-            <li class="nav-item">
-				<div class="d-flex">
-					<img class="navcrumb" src="<?php echo get_template_directory_uri(); ?>/img/crumb1.png" alt="digital bread crumb">
-				<a href="#" class="nav-link">Contact</a>
-			</div>
-			</li>
-            <li class="nav-item">
-				<div class="d-flex">
-					<img class="navcrumb" src="<?php echo get_template_directory_uri(); ?>/img/crumb2.png" alt="digital bread crumb">
-					<a href="#" class="nav-link">Blog</a>
-				</div>
-			</li>
-          </ul>
-        </div>
-	  </nav>
-
+	<nav class="navbar navbar-expand-md navbar-light" role="navigation">
+        <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+            </button>
+        <a class="navbar-brand" href="#">
+            <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/wc-site-logo.png" alt="Webcrumbs alt logo. Digital bread with WC abbreviation in the middle.">
+        </a>
+        <?php
+        wp_nav_menu( array(
+            'theme_location'    => 'primary',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'bs-example-navbar-collapse-1',
+            'menu_class'        => 'nav navbar-nav ml-auto',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker(),
+        ) );
+        ?>
+    </div>
+</nav>
+<?php
+// wp_nav_menu( array(
+//     'theme_location'  => 'primary',
+//     'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+//     'container'       => 'div',
+//     'container_class' => 'collapse navbar-collapse',
+//     'container_id'    => 'bs-example-navbar-collapse-1',
+//     'menu_class'      => 'navbar-nav mr-auto',
+//     'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+//     'walker'          => new WP_Bootstrap_Navwalker(),
+// ) );
+?>
 
 
 
