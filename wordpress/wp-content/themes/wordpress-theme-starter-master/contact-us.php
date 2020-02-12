195a0dcb59b5cc3315f46d99587906c45d3a66db
SolNat99 <natalieas01@gmail.com>
2020-02-12 15:23:16 -0800

    Added plugin contact form 7 and used css on it.

--- wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/contact-us.php
@@ -12,29 +12,29 @@ get_header(); ?>
 
         <!-- icon-text-container is to help align the icons and text together -->
         <div class="icon-text-container col-xs col-lg-4">
-            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png">
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png" alt="An icon that symbolizes locations">
             <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
         </div>
 
 
         <div class="icon-text-container col-xs col-lg-4">
-            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png">
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png" alt="An icon that depicts an envelope">
             <h2> info.gkw.wp@gmail.com </h2>
         </div>
 
 
 
         <div class="icon-text-container col-xs col-lg-4">
-            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/phone.png">
+            <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/phone.png" alt="An icon that depicts a cellphone">
             <h2> ###-###-####</h2>
         </div>
 
+        <?php echo do_shortcode( '[contact-form-7 id="39" title="Contact form 1"]' ); ?>
 
-
-        <div class="icon-text-container col-xs col-12 text-center">
+        <!--<div class="icon-text-container col-xs col-12 text-center">
             <h2>Have ideas? Send them to Us!</h2>
             <button type="button" class="btn btn-primary btn-lg">Send Ideas</button>
-        </div>
+        </div>-->
 
     </div> <!-- end row -->
 </div> <!-- end container -->
